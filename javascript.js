6f76caf1f8646eb24f9e8e2bfb1bcac673453b6a
monsalex23 <monsalex23@gmail.com>
2022-05-06 15:39:27 -0300

    add função colisao

--- javascript.js
+++ javascript.js
@@ -144,7 +144,7 @@ function start() {
             $("#disparo").css("top", topoTiro);
             $("#disparo").css("left", tiroX);
 
-            var tempoDisparo = window.setInterval(executaDisparo, 30);
+            let tempoDisparo = window.setInterval(executaDisparo, 30);
 
         } //Fecha podeAtirar
 
@@ -165,13 +165,44 @@ function start() {
     } // Fecha disparo()
 
 
-    function colisao() {   //usando jquery collision
+    function colisao() {
         let colisao1 = ($("#jogador").collision($("#inimigo1")));
         // jogador com o inimigo1
 
-        console.log(colisao1);
+        if (colisao1.length > 0) {
+
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X, inimigo1Y);
+
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
 
     } //Fim da função colisao()
+
+    //Explosão 1
+    function explosao1(inimigo1X, inimigo1Y) {
+        $("#fundoGame").append("<div id='explosao1'></div");
+        $("#explosao1").css("background-image", "url(assets/imgs/explosao.png)");
+        let div = $("#explosao1");
+        div.css("top", inimigo1Y);
+        div.css("left", inimigo1X);
+        div.animate({ width: 200, opacity: 0 }, "slow");
+
+        let tempoExplosao = window.setInterval(removeExplosao, 1000);
+
+        function removeExplosao() {
+
+            div.remove();
+            window.clearInterval(tempoExplosao);
+            tempoExplosao = null;
+
+        }
+
+    } // Fim da função explosao1()
+
 
 
 }
